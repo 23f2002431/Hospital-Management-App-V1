dafa3259ec1df75bdf66e9517fc69b1c462a23ae
Rigveda <rigveda26@gmail.com>
2025-11-16 00:53:10 +0530

    : Milestone-HMS Auth-RBAC

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask import render_template,request,redirect,url_for,session
 
 
 app= Flask(__name__)
@@ -10,10 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 #initialize the database
 db = SQLAlchemy(app)
-
-
-
-
 
 
 #####---------------------- Models -----------------------#############
@@ -64,6 +61,61 @@ class Treatment(db.Model):
     treat_name=db.Column(db.String(100))
     description=db.Column(db.Text)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/registration',methods=["POST","GET"])
+def registration():
+    if request.method=="POST":
+        name=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        
+        user=User.query.filter_by(username=name,email=email).first()
+        if user:
+            return redirect(url_for('login'))
+        new_user=User(username=name,email=email,password=password,role="patient")
+        db.session.add(new_user)
+        db.session.commit()
+ 
+        return redirect(url_for('login'))
+    return render_template('registration.html')
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    if request.method=="POST":
+        name=request.form['frontend_name']
+        password=request.form['frontend_password']
+
+        user=User.query.filter_by(username=name,password=password).first()
+        if user and user.role=="patient":
+            return redirect(url_for('patient_dashboard'))
+        
+        elif user and user.role=="doctor":
+            return redirect(url_for('doctor_dashboard'))
+        
+        elif user and user.role=="admin":
+            return redirect(url_for('admin_dashboard'))
+        return render_template('login.html',error_message="You are new user please register first")
+    return render_template('login.html')
+
+
+
+
+@app.route('/patient_dashboard')
+def patient_dashboard ():
+    return render_template('patient_dashboard.html')
+
+
+@app.route('/doctor_dashboard')
+def doctor_dashboard():
+    return render_template('doctor_dashboard.html')
+
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
 
 
 #run the app and create a database
@@ -83,5 +135,6 @@ if __name__=='__main__':
             db.session.add(admin_db)
             db.session.commit()
     app.run(debug=True)
+
 
 
